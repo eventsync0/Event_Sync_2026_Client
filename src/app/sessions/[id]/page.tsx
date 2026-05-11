02372f1e55eedd080c66d1c9a38377f005483109
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-11 18:50:27 +0300

    refactor(sessions): change the throw new error to notFound

--- src/app/sessions/[id]/page.tsx
+++ src/app/sessions/[id]/page.tsx
@@ -1,4 +1,10 @@
-export default async function SessionPage({ params }: { params: { id: string } }) {
+import { notFound } from "next/navigation";
+
+export default async function SessionPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const API_URL = "http://localhost:3001/api";
 
   const res = await fetch(`${API_URL}/sessions/${params.id}`, {
@@ -6,15 +12,32 @@ export default async function SessionPage({ params }: { params: { id: string } }
   });
 
   if (!res.ok) {
-    throw new Error("Error while loading session");
-  }
+    notFound();
+    }
 
   const json = await res.json();
   const session = json.data;
 
   return (
-    <div>
-      <h1>{session.title}</h1>
+    <div className="max-w-4xl mx-auto px-6 py-10">
+
+      {/* TITLE */}
+      <h1 className="text-3xl font-bold text-black">
+        {session.title}
+      </h1>
+
+      {/* DESCRIPTION */}
+      <p className="text-gray-600 mt-4 text-lg leading-relaxed">
+        {session.description}
+      </p>
+
+      {/* INFO BLOCK */}
+      <div className="mt-6 p-4 border rounded-lg bg-gray-50">
+        <p className="text-sm text-gray-700">
+          📅 Date : <span className="font-medium">{session.date}</span>
+        </p>
+      </div>
+
     </div>
   );
 }
