93305c1ce0331e9ae49b2e037848b7d4fa1f52e4
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-06-08 16:36:28 +0300

    chore(speaker): set to the HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import EventSection from '@/components/sections/EventSection';
 import HeroSection from '@/components/sections/HeroSection';
+import SpeakerSection from '@/components/sections/SpeakerSection';
 
 export default function HomePage() {
   return (
     <div className="">
       <HeroSection />
       <EventSection />
+      <SpeakerSection />
     </div>
   );
 }
